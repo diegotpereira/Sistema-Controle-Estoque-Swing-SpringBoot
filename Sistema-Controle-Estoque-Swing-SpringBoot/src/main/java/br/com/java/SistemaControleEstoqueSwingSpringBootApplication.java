c8a1c2a77f44f9131f5a9fba082e0f6b26cdb34f
Diego <diegoteixeirapereira@hotmail.com>
2020-09-30 17:06:52 -0300

    criação do arquivo classe principal do projeto

---
+++ Sistema-Controle-Estoque-Swing-SpringBoot/src/main/java/br/com/java/SistemaControleEstoqueSwingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaControleEstoqueSwingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaControleEstoqueSwingSpringBootApplication.class, args);
+	}
+
+}
