0b1138dcad7f6ec529217d818e3a0a9fcc168721
Diego <diegoteixeirapereira@hotmail.com>
2020-10-01 10:52:08 -0300

    alteração no arquivo classe MercadoriaTable.java criação do metodo getMercadoriaAt

--- Sistema-Controle-Estoque-Swing-SpringBoot/src/main/java/br/com/java/view/MercadoriaTable.java
+++ Sistema-Controle-Estoque-Swing-SpringBoot/src/main/java/br/com/java/view/MercadoriaTable.java
@@ -2,6 +2,21 @@ package br.com.java.view;
 
 import javax.swing.JTable;
 
+import br.com.java.model.Mercadoria;
+
+
+
+
 public class MercadoriaTable extends JTable {
+	
+	private MercadoriaTableModel modelo;
+	
+	public Mercadoria getMercadoriaSelected() {
+		int i = getSelectedRow();
+		if (i < 0) {
+			return null;
+		}
+		return modelo.getMercadoriaAt(i);
+	}
 
 }
