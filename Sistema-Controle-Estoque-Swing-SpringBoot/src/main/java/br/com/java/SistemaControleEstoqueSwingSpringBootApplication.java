bcbaa2c5bf5f426229b1bbb8519e2b12ce1295e0
Diego <diegoteixeirapereira@hotmail.com>
2020-10-01 10:32:49 -0300

    alteração no arquivo classe principal metodo man

--- Sistema-Controle-Estoque-Swing-SpringBoot/src/main/java/br/com/java/SistemaControleEstoqueSwingSpringBootApplication.java
+++ Sistema-Controle-Estoque-Swing-SpringBoot/src/main/java/br/com/java/SistemaControleEstoqueSwingSpringBootApplication.java
@@ -1,13 +1,18 @@
 package br.com.java;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.java.view.ListaMercadoriasFrame;
+
+
 
 @SpringBootApplication
 public class SistemaControleEstoqueSwingSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaControleEstoqueSwingSpringBootApplication.class, args);
+		//SpringApplication.run(SistemaControleEstoqueSwingSpringBootApplication.class, args);
+		new ListaMercadoriasFrame();
 	}
 
 }
