43579510550265f4044a45ef6f8ed1c6e3ebbcec
Diego <diegoteixeirapereira@hotmail.com>
2020-10-01 10:54:58 -0300

    aletração no arquivo MenuF1 criação do metodo MenuF1

--- Sistema-Controle-Estoque-Swing-SpringBoot/src/main/java/br/com/java/view/MenuF1.java
+++ Sistema-Controle-Estoque-Swing-SpringBoot/src/main/java/br/com/java/view/MenuF1.java
@@ -1,5 +1,14 @@
 package br.com.java.view;
 
-public class MenuF1 {
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.KeyStroke;
+
+public class MenuF1 extends JMenu {
+	
+	public MenuF1(String title) {
+		super(title);
+		this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("F1"),"click");
+	}
 
 }
